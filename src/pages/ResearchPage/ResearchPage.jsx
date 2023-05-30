2b739e2061c985d0b3d2a72f6527bf282a33b751
Floriane Pelissier <flopel@MacBook-Pro-de-Floriane.local>
2023-05-30 18:06:02 +0200

    add marker and popup

--- src/pages/ResearchPage/ResearchPage.jsx
+++ src/pages/ResearchPage/ResearchPage.jsx
@@ -1,9 +1,13 @@
+import "./researchPage.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./researchPage.css";
+
 import displaystars from "../../utils/displaystars";
+import veganlogo from "../../assets/img/category_vegan.png";
+import veggielogo from "../../assets/img/category_vegetarian.png";
+import vegoptlogo from "../../assets/img/category_veg-options.png";
 
 import veganIcon from "../../assets/img/vegan_marker.png";
 import vegeIcon from "../../assets/img/vegetarian_marker.png";
@@ -11,39 +15,48 @@ import vegOption from "../../assets/img/veg_options_marker.png";
 import vegShop from "../../assets/img/veg_shop_marker.png";
 import otherLogo from "../../assets/img/other_marker.png";
 
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 const research = ({ handleSearch }) => {
   const location = useLocation();
   let research = location.state.research;
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [IsLoading, setIsLoading] = useState(true);
   const [counter, setCounter] = useState(0);
   const numberOfPage = Math.ceil(counter / 81);
+  console.log(data.type);
+  let iconImg = "";
 
-  // let iconImg = "";
+  if (data.type === "vegan") {
+    iconImg = veganIcon;
+  } else if (data.type === "vegetarian") {
+    iconImg = vegeIcon;
+  } else if (data.type === "veg-options") {
+    iconImg = vegOption;
+  } else if (data.type === "Veg Store") {
+    iconImg = vegShop;
+  } else {
+    iconImg = otherLogo;
+  }
 
-  // if (data.type === "vegan") {
-  //   iconImg = veganIcon;
-  // } else if (data.type === "vegetarian") {
-  //   iconImg = vegeIcon;
-  // } else if (data.type === "veg-options") {
-  //   iconImg = vegOption;
-  // } else if (data.type === "Veg Store") {
-  //   iconImg = vegShop;
-  // } else {
-  //   iconImg = otherLogo;
-  // }
-
-  // const icon = new L.Icon({
-  //   iconUrl: iconImg,
-  //   iconSize: [30, 35],
-  // });
+  const icon = new L.Icon({
+    iconUrl: iconImg,
+    iconSize: [30, 35],
+  });
 
   const displayPage = (numberOfPage) => {
+    let page = 1;
     const tab = [];
     if (numberOfPage > 1) {
       for (let i = 0; i <= numberOfPage; i++) {
-        tab.push(<button onClick={console.log("clic")}>{i + 1}</button>);
+        tab.push(
+          <button
+            onClick={() => {
+              page = page + 1;
+            }}
+          >
+            {i + 1}
+          </button>
+        );
       }
     } else tab.push(<button>1</button>);
     return tab;
@@ -56,6 +69,7 @@ const research = ({ handleSearch }) => {
           `http://localhost:3000/restaurants/country?address=${research}`
         );
         console.log(response.data);
+        console.log(data.type);
         setData(response.data.restaurants);
         setCounter(response.data.count);
         setIsLoading(false);
@@ -78,19 +92,32 @@ const research = ({ handleSearch }) => {
             <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
           </button>
         </span>
-        <div>
-          <button>Végan</button> <button>Vegetarian</button>
-          <button>Veg-options</button>
+        <div className="filter">
+          <button
+            onClick={() => {
+              type = "vegan";
+            }}
+          >
+            <img src={veganlogo} alt="logo vegan" />
+            Végan
+          </button>
+          <button>
+            <img src={veggielogo} alt="logo végé" /> Vegetarian
+          </button>
+          <button>
+            <img src={vegoptlogo} alt="logo végéOptions" />
+            Veg-options
+          </button>
         </div>
-
         <nav>{displayPage(numberOfPage)}</nav>
         <div className="wrap2">
           {data.map((elem) => {
             return (
-              <div className="card" key={elem.id}>
-                <img src={elem.pictures[0]} alt="premier photo" />
-                <p>{elem.name}</p>
+              <div className="card">
+                <img src={elem.pictures[0]} alt="premiere photo" />
+                <h5>{elem.name}</h5>
                 <p>{displaystars(elem.rating)}</p>
+                <p>{elem.type}</p>
               </div>
             );
           })}
@@ -113,8 +140,15 @@ const research = ({ handleSearch }) => {
                 return (
                   <Marker
                     position={[elem.location.lat, elem.location.lng]}
-                    // icon={icon}
-                  />
+                    icon={icon}
+                  >
+                    <Popup className="custom-popup">
+                      <img src={elem.pictures[0]} alt="img" />
+                      <h4>{elem.name}</h4>
+                      <p>{displaystars(elem.rating)}</p>
+                      <h3>{elem.address}</h3>
+                    </Popup>
+                  </Marker>
                 );
               })}
             </div>
